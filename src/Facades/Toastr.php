e5538f7904d91115e296fbca3216954228dba794
孙德彪 <sundebiao@100tal.com>
2016-07-07 13:41:58 +0800

    add package source files.

---
+++ src/Facades/Toastr.php
@@ -0,0 +1,12 @@
+<?php
+namespace Meow\Toastr\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Toastr extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'toastr';
+    }
+}
