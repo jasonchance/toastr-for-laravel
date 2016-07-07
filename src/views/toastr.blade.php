0496ffc675d04eeca1b12f62c269eaae128f557d
孙德彪 <sundebiao@100tal.com>
2016-07-07 17:21:19 +0800

    fix toastr template

--- src/views/toastr.blade.php
+++ src/views/toastr.blade.php
@@ -1,3 +1,7 @@
 <link href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
 <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
-<script type="text/javascript">{!! $javascript !!}</script>
+<script type="text/javascript">
+	$(function() {
+		{!! $javascript !!}
+	});
+</script>
